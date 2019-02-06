97c3388d862a8e97dba9b22322313f01d4a8c63f
Tim Green <tiimgreen@gmail.com>
2019-02-06 18:23:31 +0000

    add in confusion matrix and other metrics

--- evaluation.py
+++ evaluation.py
@@ -7,10 +7,12 @@ clean_dataset = np.loadtxt('wifi_db/clean_dataset.txt')
 noisy_dataset = np.loadtxt('wifi_db/noisy_dataset.txt')
 room_index = 7
 
-def get_cr(tree, data):
+def get_confusion_matrix(tree, data):
     labels_predictions = [(prediction(tree, row), row[room_index]) for row in data]
-    cm = confusion_matrix(labels_predictions)
+    return confusion_matrix(labels_predictions)
 
+def get_cr(tree, data):
+    cm = get_confusion_matrix(tree, data)
     cr = 0
 
     for i in range(4):
@@ -36,6 +38,12 @@ def evaluation(dataset):
     avg_pruned = np.array([])
     avg_unpruned = np.array([])
 
+    cm = np.zeros((4, 4), dtype=int)
+    avg_cr = np.array([])
+    avg_precision = np.array([])
+    avg_recall = np.array([])
+    avg_f1 = np.array([])
+
     for test_i in range(k):
         print("Splitting into training & validation fold and test fold...")
         # split the data into training + validation, test
@@ -49,30 +57,53 @@ def evaluation(dataset):
             (training_data, validation_data) = k_fold_split(training_validation_data, k - 1, validation_i)
 
             # build the model with the training data
-            (trained_tree, _) = decision_tree_learning(training_data)
+            (trained_tree, depth_before) = decision_tree_learning(training_data)
             test_score_before = get_cr(trained_tree, test_data)
             unpruned_test_scores = np.append(unpruned_test_scores, test_score_before)
 
             # prune the tree
-            (pruned_tree, depth) = prune_tree(trained_tree, validation_data)
+            (pruned_tree, depth_after) = prune_tree(trained_tree, validation_data)
 
             # get classification rate on pruned tree
             test_score_after = get_cr(pruned_tree, test_data)
             pruned_test_scores = np.append(pruned_test_scores, test_score_after)
 
-        pruned_avg = np.mean(pruned_test_scores)
+            # calculate metrics
+            new_cm = get_confusion_matrix(pruned_tree, test_data)
+            cm = np.add(cm, new_cm)
+
+            
+
+            avg_cr = np.append(avg_cr, test_score_after)
+            avg_precision = np.append(avg_precision, test_score_after)
+            avg_recall = np.append(avg_recall, recall(new_cm))
+
         unpruned_avg = np.mean(unpruned_test_scores)
-        avg_dif = pruned_avg - unpruned_avg
+        pruned_avg = np.mean(pruned_test_scores)
+        diff = np.subtract(pruned_avg, unpruned_avg)
+        avg_dif = np.mean(diff)
         avg_pruned = np.append(avg_pruned, pruned_avg)
         avg_unpruned = np.append(avg_unpruned, unpruned_avg)
         avg_difs = np.append(avg_difs, avg_dif)
+
         print("********* RESULT **********")
-        print("Average difference between pruned and unpruned: " + str(avg_dif) + "\n\n")
+        print("average unpruned test data cr: " + str(np.mean(unpruned_test_scores)))
+        print("average pruned test data cr: " + str(np.mean(pruned_test_scores)))
+        print("Average difference between pruned and unpruned: " + str(avg_dif))
+
+    average_cm = np.true_divide(cm, k * (k - 1))
 
     print("************************ FINAL RESULT **********************")
     print("average unpruned classification rate: " + str(np.mean(avg_unpruned)))
     print("average pruned classification rate: " + str(np.mean(avg_pruned)))
     print("average differences across all 10 test folds: " + str(np.mean(avg_difs)))
+    print("average classification rate: " + str(np.mean(avg_cr)))
+    print("average precision: " + str(np.mean(avg_precision)))
+    print("average recall: " + str(np.mean(avg_recall)))
+    print("average f1: " + str(np.mean(avg_f1)))
+    print("average confusion matrix:")
+    print(average_cm)
+
 
 def prune_tree(node, validation_data, parent=None, parent_side=None, root=None, depth=0):
     if root is None:
@@ -113,7 +144,7 @@ def prune_tree(node, validation_data, parent=None, parent_side=None, root=None, 
         else:
             # classification rate improved, replace pruned node with newly
             # created leaf
-            return (new_leaf, max(depth_left, depth_right))
+            return (new_leaf, max(depth_left, depth_right) - 1)
 
     # not a node with 2 leafs simply return here
     return (node, max(depth_left, depth_right))
